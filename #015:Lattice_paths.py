ebc833facfb19814a9cdbd8092f25014df9337e0
irhesri <rhesri.1337@gmail.com>
2023-05-30 17:57:34 +0100

    update 15

--- #015:Lattice_paths.py
+++ #015:Lattice_paths.py
@@ -1,7 +1,8 @@
 def factorial (N):
-    if (N <= 1):
-        return (1);
-    return (N * factorial(N - 1));
+    res = 1;
+    for i in range (2, N + 1):
+        res *= i;
+    return (res);
 
 x = int(input());
 
@@ -9,6 +10,5 @@ for i in range (x):
     grid = input().split();
     m = int(grid[0]);
     n = int(grid[1]);
-
     res = factorial(n + m) // (factorial(m) * factorial(n));
     print (res % (10**9 + 7));
